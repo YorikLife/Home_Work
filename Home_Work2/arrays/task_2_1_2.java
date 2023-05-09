a1ca28475769e9768e633dcd1340db8bd402f5da
Denis Yordanov <blazzer100500@gmail.com>
2023-05-09 12:23:06 +0300

    fix structure bug

--- Home_Work2/arrays/task_2_1_2.java
+++
@@ -1,9 +0,0 @@
-package Home_Work2.arrays;
-import java.util.Arrays;
-import static Home_Work2.utils.ArraysUtils.arrayRandom;
-public class task_2_1_2 {
-    public static void main(String[] args) {
-        int[] container = arrayRandom(5,100);
-        System.out.println(Arrays.toString(container));
-    }
-}
