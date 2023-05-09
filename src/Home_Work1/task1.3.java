449f25c88df5044db58721fd7c9e5106d28a5363
Denis <blazzer100500@gmail.com>
2023-05-09 13:47:32 +0300

    Исправил task1.3

---
+++ src/Home_Work1/task1.3.java
@@ -0,0 +1,20 @@
+package Home_Work1;
+
+public class task1.3 {
+        public static void main(String[] args) {
+            float f = 42.5F;
+            System.out.println("Пытаемся перевести в двоичную систему число : " + f);
+            System.out.println("умножаем его на 2 = " + f*2);
+            float f1 = 85F;
+            System.out.println(f1 +" = " + Integer.toBinaryString((int) f1)); // число 85 в двоичной системе.
+            float f2 = f1/2;
+            System.out.println("делим 85 на 2 = " + (f2));/* при попытке перевести результат 42.5 в двоичную
+            систему - java выдает ошибку, т.к. можно переводить только целые числа. */
+            double d1 = 42d;// следовательно идет другим путём.
+            System.out.println("42 = " + Integer.toBinaryString((int) d1)); // число 42 в двоичной системе.
+            double d2 = 0.5d;
+            System.out.println("0.5 = " +  d2*2);//число 0.5 в двоичной системе.
+            /*можно сделать вывод, что число 42.5 в двоичной системе будет выглядеть вот таким образом:
+            101010.1*/
+        }
+    }
